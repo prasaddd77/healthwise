973acb7385a47b5cfee9ea694a622bd8dddc1fdd
Nidhi <nidhi.task@gmail.com>
2023-04-06 01:50:09 +0530

    final

--- IrisApp/views.py
+++ IrisApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from joblib import load
 import numpy as np
 import openai
-openai.api_key = "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"
+openai.api_key = "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"
 rfc_model_new = load("./savedModels/rfc_model_new.joblib")
 wt_knn = load("./savedModels/wt_knn.joblib")
 symptoms = load("./savedModels/symptoms.joblib")
@@ -29,19 +29,25 @@ def diseasePrediction(request):
 
                 # Print the sliced dictionary
                 return sliced_dict
-            def final_op(wt_knnl_dict, rfc_new_model_dict):
-                final_op = {}
-                for key, val in wt_knnl_dict.items():
-                    final_op[key] = val
+            def final_op(dict1, dict2):
+                diseases = set(dict1.keys()) | set(dict2.keys())
+                priors = {disease: 1.0/len(diseases) for disease in diseases}
+                posterior = {}
+                for disease in diseases:
+                    if disease in dict1 and disease in dict2:
+                        posterior[disease] = dict1[disease] * dict2[disease] * priors[disease]
+                    elif disease in dict1:
+                        posterior[disease] = dict1[disease] * priors[disease]
+                    elif disease in dict2:
+                        posterior[disease] = dict2[disease] * priors[disease]
+                total_prob = sum(posterior.values())
+                for disease in posterior:
+                    posterior[disease] /= total_prob
+                sorted_dict = dict(sorted(posterior.items(), key=lambda item: item[1], reverse=True))
 
-                for key, val in rfc_new_model_dict.items():
-                    if key not in final_op:
-                        final_op[key] = val
-                    else:
-                        v1 = final_op[key]
-                        ans = (v1+val)/2
-                        final_op[key] = ans
-                return final_op
+                for key, val in sorted_dict.items():
+                    sorted_dict[key] = round(val*100,3)
+                return sorted_dict
             new_data = symptoms_list
             input_list = []
             for i in symptoms:
